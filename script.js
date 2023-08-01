344f2d5c2266b45377b71cf4cd2535b830b7b152
gedeonge1 <136656685+gedeonge1@users.noreply.github.com>
2023-08-01 16:25:18 +0200

    function that takes two argument at a time and return the result

---
+++ script.js
@@ -0,0 +1,17 @@
+//function that takes two argument at time and return the result
+function add(num1,num2){
+    num1 += num2
+    return num1
+   }
+function substract(num1,num2){
+    num1 -= num2
+    return num1
+}
+function multiply(num1,num2){
+   num1 *= num2
+   return num1
+}
+function divide(num1,num2){
+    num1 /= num2
+    return num1
+}
